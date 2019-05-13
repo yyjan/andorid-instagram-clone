4f2767ea3e6701453b56ead4adbb0dc55a9202a9
yun <yyjan219@gmail.com>
2019-05-13 14:28:19 +0900

    feat: inital commit

---
+++ app/src/main/java/com/example/yun/yunstagram/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.yun.yunstagram
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var textMessage: TextView
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                textMessage.setText(R.string.title_home)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_search -> {
+                textMessage.setText(R.string.title_search)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_control -> {
+                textMessage.setText(R.string.title_control)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_favorite -> {
+                textMessage.setText(R.string.title_favorite)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_account -> {
+                textMessage.setText(R.string.title_account)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        textMessage = findViewById(R.id.message)
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+    }
+}
